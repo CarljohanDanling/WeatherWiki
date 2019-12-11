c3322212d63b8809ac8e72abf1fecfdb1bec45a4
Carljohan Danling <carljohan.danling@gmail.com>
2019-12-11 14:22:23 +0100

    Adds structure for current weather.

--- UserControls/CurrentWeather.xaml.cs
+++ UserControls/CurrentWeather.xaml.cs
@@ -15,11 +15,11 @@ using Windows.UI.Xaml.Navigation;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
-namespace WeatherWiki
+namespace WeatherWiki.UserControls
 {
-    public sealed partial class Forecast : UserControl
+    public sealed partial class CurrentWeather : UserControl
     {
-        public Forecast()
+        public CurrentWeather()
         {
             this.InitializeComponent();
         }
---
+++ UserControls/Forecast.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace WeatherWiki.UserControls
+{
+    public sealed partial class Forecast : UserControl
+    {
+        public Forecast()
+        {
+            this.InitializeComponent();
+
+            ObservableCollection<ForecastDay> forecastDays = new ObservableCollection<ForecastDay>
+            {
+                new ForecastDay()
+                {
+                    Day = "Wed 28",
+                    ImagePath = "/Images/WeatherState/a01d.png",
+                    Temperature = "1˚, -5˚",
+                    Condition = "Windy"
+                },
+                new ForecastDay()
+                {
+                    Day = "Tue 12",
+                    ImagePath = "/Images/WeatherState/c04d.png",
+                    Temperature = "1˚, -5˚",
+                    Condition = "Rain"
+                },
+                new ForecastDay()
+                {
+                    Day = "Sun 3",
+                    ImagePath = "/Images/WeatherState/d01d.png",
+                    Temperature = "1˚, -5˚",
+                    Condition = "Clear"
+                },
+                new ForecastDay()
+                {
+                    Day = "Fri 22",
+                    ImagePath = "/Images/WeatherState/f01d.png",
+                    Temperature = "1˚, -5˚",
+                    Condition = "Windy"
+                },
+                new ForecastDay()
+                {
+                    Day = "Thu 4",
+                    ImagePath = "/Images/WeatherState/s03d.png",
+                    Temperature = "1˚, -5˚",
+                    Condition = "Rain"
+                },
+                                new ForecastDay()
+                {
+                    Day = "Wed 7",
+                    ImagePath = "/Images/WeatherState/s01d.png",
+                    Temperature = "1˚, -5˚",
+                    Condition = "Rain"
+                },
+                                                new ForecastDay()
+                {
+                    Day = "Sat 29",
+                    ImagePath = "/Images/WeatherState/t02n.png",
+                    Temperature = "1˚, -5˚",
+                    Condition = "Rain"
+                }
+            };
+
+            observableColletionForecast.ItemsSource = forecastDays;
+        }
+    }
+
+    public class ForecastDay
+    {
+        public string Day { get; set; }
+        public string ImagePath { get; set; }
+        public string Temperature { get; set; }
+        public string Condition { get; set; }
+    }
+}
